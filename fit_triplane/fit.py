fbb53eb702542596a20d3eb74157f8cab01c4400
Kai-Chen Tung <dadado0570@gmail.com>
2025-07-05 15:24:28 -0700

    Revise Triplane implementation for four triplanes test

--- fit_triplane/fit.py
+++ fit_triplane/fit.py
@@ -114,7 +114,19 @@ class Triplane(nn.Module):
             # torch.nn.init.normal_(sdf_proxy[2].weight, 0.0, np.sqrt(2) / np.sqrt(channel))
             torch.nn.init.kaiming_normal_(sdf_proxy[2].weight, a=0, mode='fan_out', nonlinearity='relu')
 
-            ini_sdf = torch.zeros([3, channel, reso, reso])
+            sdf_proxy_2 = nn.Sequential(
+                nn.Linear(3, channel * 2), nn.ReLU(),
+                nn.Linear(channel * 2, channel * 2),
+            )
+            torch.nn.init.constant_(sdf_proxy_2[0].bias, 0.0)
+            # torch.nn.init.normal_(sdf_proxy[0].weight, 0.0, np.sqrt(2) / np.sqrt(channel))
+            torch.nn.init.kaiming_normal_(sdf_proxy_2[0].weight, a=0, mode='fan_out', nonlinearity='relu')
+            torch.nn.init.constant_(sdf_proxy_2[2].bias, 0.0)
+            # torch.nn.init.normal_(sdf_proxy[2].weight, 0.0, np.sqrt(2) / np.sqrt(channel))
+            torch.nn.init.kaiming_normal_(sdf_proxy_2[2].weight, a=0, mode='fan_out', nonlinearity='relu')
+
+            ini_sdf_1 = torch.zeros([1, channel * 2, reso, reso])
+            ini_sdf_23 = torch.zeros([2, channel, reso, reso])
             # create XY, XZ, YZ planes grid coordinates to initialize the triplane's weights
             # since grid_sample expects the input to be in the range [-1, 1]
             # we create a grid of points in the range [-1, 1] for each plane
@@ -126,11 +138,13 @@ class Triplane(nn.Module):
             inputz = torch.stack([U, V, Z], -1).reshape(-1, 3)
             # use permute to make the channel dimension as first dimension, batch size as second dimension
             # and reshape back to grid format
-            ini_sdf[0] = sdf_proxy(inputx).permute(1, 0).reshape(channel, reso, reso)
-            ini_sdf[1] = sdf_proxy(inputy).permute(1, 0).reshape(channel, reso, reso)
-            ini_sdf[2] = sdf_proxy(inputz).permute(1, 0).reshape(channel, reso, reso)
+            # ini_sdf[0] = sdf_proxy(inputx).permute(1, 0).reshape(channel, reso, reso)
+            ini_sdf_1[0] = sdf_proxy_2(inputx).permute(1, 0).reshape(channel * 2, reso, reso)
+            ini_sdf_23[0] = sdf_proxy(inputy).permute(1, 0).reshape(channel, reso, reso)
+            ini_sdf_23[1] = sdf_proxy(inputz).permute(1, 0).reshape(channel, reso, reso)
             # unsqueeze and repeat command just add a new dim and repeat n times at the 1st dim
-            self.triplane = torch.nn.Parameter(ini_sdf.unsqueeze(0).repeat(self.n, 1, 1, 1, 1) / 3, requires_grad=True)
+            self.triplane_1 = torch.nn.Parameter(ini_sdf_1.unsqueeze(0).repeat(self.n, 1, 1, 1, 1) / 4 * 2, requires_grad=True)
+            self.triplane_23 = torch.nn.Parameter(ini_sdf_23.unsqueeze(0).repeat(self.n, 1, 1, 1, 1) / 4, requires_grad=True)
         elif init_type == "zero_init":
             self.triplane = torch.nn.Parameter(torch.zeros([self.n, 3, channel, reso, reso]), requires_grad=True)
 
@@ -185,25 +199,43 @@ class Triplane(nn.Module):
         # xyz shape: [sample_batch_size, 3 (xyz coords)]
         # pts: [M,3]
         M, _ = xyz.shape
-        plane_features = self.triplane[oid:oid + 1].view(3, self.C, self.R, self.R)
+        # plane_features = self.triplane[oid:oid + 1].view(3, self.C, self.R, self.R)
+        plane_features = [self.triplane_1[oid][0:], self.triplane_23[oid][0:1], self.triplane_23[oid][1:2]]
         projected_coordinates = self.project_onto_planes(xyz).unsqueeze(1)
-        feats = F.grid_sample(
-            plane_features,  # [3,C,R,R]
-            projected_coordinates.float(),  # [3,1,M,2]
-            mode="bilinear",
-            padding_mode="zeros",
-            align_corners=True
-        )  # [3,C,1,M]
-        feats = feats.permute(0, 3, 2, 1).reshape(3, M, self.C).sum(0)
+        # feats = F.grid_sample(
+        #     plane_features,  # [3,C,R,R]
+        #     projected_coordinates.float(),  # [3,1,M,2]
+        #     mode="bilinear",
+        #     padding_mode="zeros",
+        #     align_corners=True
+        # )  # [3,C,1,M]
+        # feats = feats.permute(0, 3, 2, 1).reshape(3, M, self.C).sum(0)
+        
+        feats = []
+        for i in range(3):
+            feats.append(F.grid_sample(
+                plane_features[i],  # [1 triplane,C,R,R]
+                projected_coordinates[i:i+1].float(),  # [1 triplane,1,M,2]
+                mode="bilinear",
+                padding_mode="zeros",
+                align_corners=True
+            ))
+        feats = torch.cat([feats[0][0:,:self.C], feats[0][0:,self.C:], feats[1][0:], feats[2][0:]], dim=0).permute(0, 3, 2, 1).reshape(4, M, self.C).sum(0)
         return feats  # [M,C]
 
     def update_resolution(self, new_reso):
-        old_tri = self.triplane.data.view(self.n * 3, self.C, self.R, self.R)
-        new_tri = F.interpolate(old_tri, size=(new_reso, new_reso), mode='bilinear', align_corners=True)
+        # old_tri = self.triplane.data.view(self.n * 3, self.C, self.R, self.R)
+        # new_tri = F.interpolate(old_tri, size=(new_reso, new_reso), mode='bilinear', align_corners=True)
+        # self.R = new_reso
+        # self.triplane = torch.nn.Parameter(new_tri.view(self.n, 3, self.C, self.R, self.R), requires_grad=True)
+        old_tri_1 = self.triplane_1.data.view(self.n * 1, self.C * 2, self.R, self.R)
+        old_tri_23 = self.triplane_23.data.view(self.n * 2, self.C, self.R, self.R)
+        new_tri_1 = F.interpolate(old_tri_1, size=(new_reso, new_reso), mode='bilinear', align_corners=True)
+        new_tri_23 = F.interpolate(old_tri_23, size=(new_reso, new_reso), mode='bilinear', align_corners=True)
         self.R = new_reso
-        self.triplane = torch.nn.Parameter(new_tri.view(self.n, 3, self.C, self.R, self.R), requires_grad=True)
-
-
+        self.triplane_1 = torch.nn.Parameter(new_tri_1.view(self.n, 1, self.C * 2, self.R, self.R), requires_grad=True)
+        self.triplane_23 = torch.nn.Parameter(new_tri_23.view(self.n, 2, self.C, self.R, self.R), requires_grad=True)
+        
 class Network(nn.Module):
     def __init__(self,
                  d_in=32,
@@ -311,22 +343,34 @@ if __name__ == "__main__":
 
         # for debugging
         if epoch % 2000 == 0:
-            for dim in range(3):
+            for dim in range(2):
                 plot_single_channel(
-                    triplane[50].triplane[0][dim][16].detach(), 
+                    triplane[50].triplane_1[0][0][dim*config.channel + 16].detach(), 
+                    title=f"plane_dim_0_{dim}_epoch_{epoch}",
+                    save_path=f"plane_dim_0_{dim}_epoch_{epoch}.png"
+                )
+            for dim in range(2):
+                plot_single_channel(
+                    triplane[50].triplane_23[0][dim][16].detach(), 
                     title=f"plane_dim_{dim}_epoch_{epoch}",
                     save_path=f"plane_dim_{dim}_epoch_{epoch}.png"
                 )
 
         if epoch in config.c2f_scale:
-            new_reso = int(config.resolution / (2 ** (len(config.c2f_scale) - config.c2f_scale.index(epoch) - 1)))
             # for debugging
-            for dim in range(3):
+            for dim in range(2):
                 plot_single_channel(
-                    triplane[50].triplane[0][dim][16].detach(), 
-                    title=f"plane_dim_{dim}_reso_{new_reso}",
-                    save_path=f"plane_dim_{dim}_reso_{new_reso}.png"
+                    triplane[50].triplane_1[0][0][dim*config.channel + 16].detach(), 
+                    title=f"plane_dim_0_{dim}_reso_{triplane[50].R}",
+                    save_path=f"plane_dim_0_{dim}_reso_{triplane[50].R}.png"
                 )
+            for dim in range(2):
+                plot_single_channel(
+                    triplane[50].triplane_23[0][dim][16].detach(), 
+                    title=f"plane_dim_{dim}_reso_{triplane[50].R}",
+                    save_path=f"plane_dim_{dim}_reso_{triplane[50].R}.png"
+                )
+            new_reso = int(config.resolution / (2 ** (len(config.c2f_scale) - config.c2f_scale.index(epoch) - 1)))
             for tri in triplane:
                 tri.update_resolution(new_reso)
             optimizer = create_optimizer(net, triplane, config)
