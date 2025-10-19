affaffde431a85536b9fa292b93c0941cfe589e8
Kai-Chen Tung <kctung@sophia-login-02.pub.alcf.anl.gov>
2025-10-19 07:19:08 +0000

    Tweak VAE inference script to support DDP

--- triplane_VAE_inference.py
+++ triplane_VAE_inference.py
@@ -3,6 +3,9 @@ import torch
 import torch.nn.functional as F
 import numpy as np
 
+import torch.distributed as dist
+from torch.nn.parallel import DistributedDataParallel as DDP
+
 import logging
 import argparse
 from datetime import datetime
@@ -16,43 +19,40 @@ def parse_args():
     parser.add_argument("--expdir", type=str, default="./logs/test_hashencoding_train/20250508-024915", help="Checkpoint Directory to load the model from")
     parser.add_argument("--model_file_name", type=str, default="vae_model_epoch_9999.ckpt", help="Model file name")
     parser.add_argument("--model_config", type=str, default="model_a", help="Model config file name")
-    parser.add_argument("--n_instances", type=int, default=90, help="Number of instances in the dataset (i.e., n_timesteps)")
+    
     parser.add_argument("--pretrained_triplane_file_path", type=str, default="fit_triplane/ch_32_saved_model.ckpt", help="File Path to Pretrained Triplanes Model")
     return parser.parse_args()
 
 if __name__ == "__main__":
+    torch.cuda.set_device(int(os.environ["LOCAL_RANK"]))
+    dist.init_process_group("nccl")
+    
     args = parse_args()
     
     # model configs are stored as python scripts, import the target config here
     cfg = importlib.import_module(f"autoencoder_config.triplane.{args.model_config}")
     
-    vae_model = torch.nn.DataParallel(VAE_no_KL(cfg.vae_config, cfg.encoder_dims, cfg.feature_size_encoder, cfg.decoder_dims,
+    vae_model = DDP(VAE_no_KL(cfg.vae_config, cfg.encoder_dims, cfg.feature_size_encoder, cfg.decoder_dims,
                         cfg.feature_size_decoder, cfg.fpn_encoders_layer_dim_idx, cfg.fpn_decoders_layer_dim_idx,
                         cfg.fpn_encoders_down_idx, cfg.fpn_encoders_up_idx, cfg.fpn_decoders_down_idx,
-                        cfg.fpn_decoders_up_idx, cfg.block_config)).cuda()
+                        cfg.fpn_decoders_up_idx, cfg.block_config).cuda())
     # pretrained_vae_model = torch.load("logs/triplane_VAE_ch_32_single_volume/20250614-180226/vae_model_epoch_9999.ckpt")
     # pretrained_vae_model = torch.load("logs/triplane_AE_model_a/20250619-000758/vae_model_epoch_1399.ckpt")
     vae_model.load_state_dict(torch.load(os.path.join(args.expdir, args.model_file_name), weights_only=False)["model_state_dict"])
     
     # load pretrained tri-plane here
-    loaded_model = torch.load(args.pretrained_triplane_file_path)
-    triplane_weights = [loaded_model['triplane_state_dict'][f"{idx}.triplane"] for idx in range(args.n_instances)]
+    pretrained_triplane_model = torch.load(args.pretrained_triplane_file_path, map_location="cpu")
+    keys = pretrained_triplane_model['triplane_state_dict'].keys()
+    n_instances = sum(1 for k in keys if k.endswith("triplane"))
+    triplane_weights = [pretrained_triplane_model['triplane_state_dict'][f"{idx}.triplane"] for idx in range(n_instances)]
     triplane_weights = torch.cat(triplane_weights, dim=0)
-    # TODO: check whether it makes sense to pass vae_config["plane_shape"] into LatentWeightDataset
-    # since it is originally designed to receive latent weights in 3D space (hash grid)
-    # Need to check how does the original VAE receive input:
-    # [batch size, 3 (triplanes), #channels, res, res] or [batch size, #channels, res, res * 3 (triplanes)]
-    # seems both work(?)
-    # normalize values to -1~1 to align with the value range used in training (for only one volume)
-    # original_max = triplane_weights[50:51][0].max()
-    # original_min = triplane_weights[50:51][0].min()
-    # triplane_weights[50:51][0] = (triplane_weights[50:51][0] - original_min) / (original_max - original_min)
-    # triplane_weights[50:51][0] = triplane_weights[50:51][0] * 2 - 1
     # normalization for training all volumes
     original_min = triplane_weights.min()
     original_max = triplane_weights.max()
     triplane_weights = (triplane_weights - original_min) / (original_max - original_min)
     triplane_weights = triplane_weights * 2 - 1
+    min = -1
+    max = 1
     
     train_dataloader = torch.utils.data.DataLoader(
         LatentWeightDataset(
@@ -60,7 +60,7 @@ if __name__ == "__main__":
         cfg.vae_config["plane_shape"]),
         batch_size=1,
         shuffle=False)
-    min, max = train_dataloader.dataset.get_value_range()
+    
     value_range = max - min
     # input_tensor = torch.randn(2, 3, 32, 128, 128).cuda()
     input_tensor = next(iter(train_dataloader))[0].cuda()
@@ -69,13 +69,15 @@ if __name__ == "__main__":
     print("loss: {}".format(loss))
     print("z shape: {}".format(out[-1].shape))
     print("reconstruct shape: {}".format(out[0].shape))
-    samples = vae_model.module.sample(2)
-    print("samples shape: {}".format(samples[0].shape))
+    # samples = vae_model.module.sample(2)
+    # print("samples shape: {}".format(samples[0].shape))
+
     # to make batchnorm function correctly for inference, need to call eval
     vae_model.eval()
     with torch.no_grad():
         for batch_idx, raw_data in enumerate(train_dataloader):
             
+            raw_data[0] = raw_data[0].cuda()
             output = vae_model(raw_data[0])
             # reconstructed results is the first element of the output (output[0])
             recon_loss = F.mse_loss(output[0], raw_data[0])
@@ -88,6 +90,6 @@ if __name__ == "__main__":
             # import pdb; pdb.set_trace()
             # normalize output from -1~1 back to its original value range to align with the value range of triplane fitting
             output[0] = ((output[0] - min) / (max - min)) * (original_max - original_min) + original_min
-            loaded_model['triplane_state_dict'][f"{batch_idx}.triplane"] = output[0]
+            pretrained_triplane_model['triplane_state_dict'][f"{batch_idx}.triplane"] = output[0]
     
-    torch.save(loaded_model, os.path.join(args.expdir, f"VAE_Reconstructed_triplane.pt"))
+    torch.save(pretrained_triplane_model, os.path.join(args.expdir, f"VAE_Reconstructed_triplane.pt"))
