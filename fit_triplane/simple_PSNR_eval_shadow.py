ae3e97d32e29c331c4bfb1b7f44d23a574c1c3d9
Kai-Chen Tung <dadado0570@gmail.com>
2025-11-17 17:14:45 -0800

    Update simple PSNR eval script for analyzing data distribution of each training stage

--- fit_triplane/simple_PSNR_eval_shadow.py
+++ fit_triplane/simple_PSNR_eval_shadow.py
@@ -9,6 +9,27 @@ import json
 from fit import Triplane, Network
 import matplotlib.pyplot as plt
 from pysampler import create_sampler, decode_shadow
+from fit_shadow_randomly_generate import MLP_TCNN
+
+def cartesian_to_spherical_coords(cartesian_coords):
+    x, y, z = cartesian_coords[..., 0], cartesian_coords[..., 1], cartesian_coords[..., 2]
+    r = np.linalg.norm(cartesian_coords, axis=-1)
+    theta = np.arctan2(y, x)             # azimuth
+    phi = np.arccos(z / r)               # polar
+    return np.stack([theta, phi], axis=-1)
+
+# for debug
+def only_decode_raw_shadow(sampler, data_res, chunk_size, tfn_file_path, angle=[0.5, 0.5]):
+    targets = []
+    for coord_chunk in generate_coords_chunks(data_res, chunk_size):
+        target = torch.empty([coord_chunk.shape[0], 1]).float().cuda()
+        decode_shadow(sampler, coord_chunk, target, angle, tfn_file_path)
+        targets.append(target)
+    
+    targets = torch.cat(targets, dim=0)
+    
+    # targets.detach().cpu().numpy().astype(np.float32).tofile(f"test_shadow_volume.bin")
+    return targets
 
 def generate_coords_chunks(data_res, chunk_size, device='cuda'):
     """Yield chunks of coordinates from the full 3D grid."""
@@ -26,26 +47,57 @@ def generate_coords_chunks(data_res, chunk_size, device='cuda'):
         # allocate memory on CPU, only move to GPU when used for model inference
         yield coords[start:end].to(device)
 
-def inference(n_instances, data_res, chunk_size, value_range, triplane, net, light_dirs, tfn_file_path):
+def inference(n_instances, data_res, chunk_size, value_range, triplane, net, light_dirs, tfn_file_path, loaded_model, loaded_model_2=None):
     psnr_list = []
     with torch.no_grad():
         for batch_idx in range(n_instances):
+            net.load_state_dict(loaded_model['net_state_dict'])
+            triplane.load_state_dict(loaded_model['triplane_state_dict'])
             preds = []
-            targets = []
             for coord_chunk in generate_coords_chunks(data_res, chunk_size):
-                preds.append(net(triplane[batch_idx](coord_chunk, 0)))
-                target = torch.empty([coord_chunk.shape[0], 1]).float().cuda()
-                # TODO: need to convert light dir from radiance to normalized value (0~1)
-                decode_shadow(sampler, coord_chunk, target, light_dirs[batch_idx], tfn_file_path)
-                targets.append(target)
+                preds.append(net(triplane[batch_idx](coord_chunk, 0)).cpu())
             # outputs = net(triplane[batch_idx](coords, 0))
             # outputs = outputs.view(raw_data.shape)
             outputs = torch.cat(preds, dim=0)
+            print("finish prediction 1")
+            if loaded_model_2:
+                net.load_state_dict(loaded_model_2['net_state_dict'])
+                triplane.load_state_dict(loaded_model_2['triplane_state_dict'])
+                preds = []
+                for coord_chunk in generate_coords_chunks(data_res, chunk_size):
+                    preds.append(net(triplane[batch_idx](coord_chunk, 0)).cpu())
+                # outputs = net(triplane[batch_idx](coords, 0))
+                # outputs = outputs.view(raw_data.shape)
+                outputs_2 = torch.cat(preds, dim=0)
+            print("finish prediction 2")
+            targets = []
+            for coord_chunk in generate_coords_chunks(data_res, chunk_size):
+                target = torch.empty([coord_chunk.shape[0], 1]).float().cuda()
+                # TODO: need to convert light dir from radiance to normalized value (0~1)
+                decode_shadow(sampler, coord_chunk, target, light_dirs[batch_idx], tfn_file_path)
+                targets.append(target.cpu())
             targets = torch.cat(targets, dim=0)
+            print("finish decoding")
+            # Plot histogram
+            plt.figure(figsize=(6,4))
+            plt.hist(outputs.numpy(), bins=100, color='blue', alpha=0.8, log=True)
+            if loaded_model_2:
+                plt.hist(outputs_2.numpy(), bins=100, color='green', alpha=0.8, log=True)
+            plt.hist(targets.numpy(), bins=100, color='red', alpha=0.8, log=True)
+            plt.title("Value Distribution of Shadow Coefficient Volume")
+            plt.xlabel("Value")
+            plt.ylabel("Frequency")
+            plt.grid(True, linestyle='--', alpha=0.5)
+            plt.savefig("value_distribution_pred.png")
             loss = F.mse_loss(outputs, targets)
             PSNR = (20 * torch.log10(value_range / torch.sqrt(loss))).cpu()
             print("idx:", batch_idx, " psnr:", PSNR)
             psnr_list.append(PSNR)
+            if loaded_model_2:
+                loss = F.mse_loss(outputs_2, targets)
+                PSNR = (20 * torch.log10(value_range / torch.sqrt(loss))).cpu()
+                print("idx", batch_idx, " psnr of second model:", PSNR)
+            import pdb; pdb.set_trace()
             # ssim_list.append(structural_similarity_index_measure(outputs, raw_data, data_range=1.0).item())
             # save the GPU memory 
             del outputs, targets, loss
@@ -57,30 +109,41 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--config', type=str, default='base_timevarying.json')
     parser.add_argument('--triplane_file_path', type=str, default="../VAE_Reconstructed_triplane.pt")
-    
+    parser.add_argument('--triplane_file_path_2', type=str, default=None)
     parser.add_argument('--dims', type=int, nargs=3, default=[256, 256, 256])
     parser.add_argument('--dtype', type=str, default='float32')
     parser.add_argument('--raw_data_file_path', type=str, default="/media/data/qadwu/volume/vortices/vorts1.data")
     parser.add_argument('--tfn_file_path', type=str, default="/home/kctung/Projects/instant-vnr-pytorch/bindings/ovr/data/configs/vorts_shadow.json")
-    parser.add_argument('--n_instances', type=int, default=150, help="Number of shadow volumes to generate")
+    
     args = parser.parse_args()
 
     with open(args.config, 'r') as f:
         config = json.load(f)
     config = edict(config)
     
-    n_instances = args.n_instances
     # data_res = [128, 128, 128]
     data_res = args.dims
     chunk_size = 65536*192
 
-    net = Network(
-        d_in=config.channel,
-        d_hid=config.n_hid,
-        n_layers=config.n_layers,
-        d_out=config.n_labels,
-        init_type="geo_init",
-    ).cuda()
+    loaded_model = torch.load(args.triplane_file_path)
+    n_instances = len(loaded_model['light_dir_cartesian'])
+    
+    if args.triplane_file_path_2:
+        loaded_model_2 = torch.load(args.triplane_file_path_2)
+    else:
+        loaded_model_2 = None
+
+    # net = Network(
+    #     d_in=config.channel,
+    #     d_hid=config.n_hid,
+    #     n_layers=config.n_layers,
+    #     d_out=config.n_labels,
+    #     init_type="geo_init",
+    # ).cuda()
+    
+    net = MLP_TCNN(n_input_dims=config.channel, n_output_dims=config.n_labels,
+            n_hidden_layers=config.n_layers, n_neurons=config.n_hid,
+            activation="ReLU", output_activation="None")
 
     # instantiate multiple triplanes (each instance has its own triplane)
     triplane = [Triplane(
@@ -96,16 +159,15 @@ if __name__ == "__main__":
     
     sampler = create_sampler("structuredRegular", "cuda", dims=args.dims, dtype=args.dtype, n_channels=1, filename=args.raw_data_file_path)
     
-    loaded_model = torch.load(args.triplane_file_path)
-    # TODO: number of triplanes in loaded model should match #instances, but currently need to manually get the desired triplane
-    # should tweak diffusion inference to generate any number of triplane instances at once, then feed into here
-    import pdb; pdb.set_trace()
-    # TODO: see where to receive lighting direction, and number of lighting dirs should also match #instances
-    light_dirs = [[0.641, 0.907]]
-
-    net.load_state_dict(loaded_model['net_state_dict'])
-    triplane.load_state_dict(loaded_model['triplane_state_dict'])
-    psnr_list = inference(args.n_instances, data_res, chunk_size, value_range, triplane, net, light_dirs, args.tfn_file_path)
+    light_dirs = cartesian_to_spherical_coords(np.array(loaded_model['light_dir_cartesian']))
+    light_dirs_2 = cartesian_to_spherical_coords(np.array(loaded_model_2['light_dir_cartesian'])) if loaded_model_2 else None
+    print(f"light dirs 1: {light_dirs}")
+    print(f"light dirs 2: {light_dirs_2}")
+    
+    # normalize the spherical coordinates to 0~1 (to comply with shadow sampler)
+    light_dirs[:,0] = (light_dirs[:,0] % (2*np.pi)) / (2*np.pi)
+    light_dirs[:,1] = light_dirs[:,1] / np.pi
+    psnr_list = inference(n_instances, data_res, chunk_size, value_range, triplane, net, light_dirs, args.tfn_file_path, loaded_model, loaded_model_2)
     
     print(psnr_list)
     
